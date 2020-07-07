01d25c5af7b381c5c78a314fab1f960ef5db4bc6
testusuke <49048811+testusuke@users.noreply.github.com>
2020-07-07 19:09:55 +0900

    MailBox追加(未完成)

--- src/main/kotlin/net/testusuke/open/man10mail/MailCommand.kt
+++ src/main/kotlin/net/testusuke/open/man10mail/MailCommand.kt
@@ -1,8 +1,12 @@
 package net.testusuke.open.man10mail
 
+import net.testusuke.open.man10mail.Main.Companion.enable
+import net.testusuke.open.man10mail.Main.Companion.prefix
+import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 /**
  * Created by testusuke on 2020/07/04
@@ -11,7 +15,52 @@ import org.bukkit.command.CommandSender
 object MailCommand: CommandExecutor {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if(!sender.hasPermission(Permission.GENERAL)){
+            sendPermissionError(sender)
+            return false
+        }
 
+        if(args.isEmpty()){
+            if(sender is Player){
+                if(!enable){
+                    sendDisable(sender)
+                    return false
+                }
+                sender.sendMessage("${prefix}§aメールボックスを開きます。")
+                MailBox.openMailBox(sender)
+                return true
+            }else{
+                sendNotPlayerError(sender)
+                return false
+            }
+        }
+        //  args[0]
+        when(args[0]){
+            "help" -> {
+                sendHelp(sender)
+            }
+            "" -> {
+
+            }
+
+        }
         return false
+    }
+
+    private fun sendPermissionError(sender:CommandSender){
+        sender.sendMessage("${ChatColor.RED}You do not have permission.")
+    }
+    private fun sendNotPlayerError(sender:CommandSender){
+        sender.sendMessage("${ChatColor.RED}can not use console.")
+    }
+    private fun sendDisable(player: Player){
+        player.sendMessage("${prefix}§c§l現在利用できません。")
+    }
+
+    private fun sendHelp(sender: CommandSender){
+        val msg = """
+            
+        """.trimIndent()
+        sender.sendMessage(msg)
     }
 }
