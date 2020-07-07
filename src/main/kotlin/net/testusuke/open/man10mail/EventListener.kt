5cd9b2ed0134103c0c7e5594e70aef03050d73f8
testusuke <49048811+testusuke@users.noreply.github.com>
2020-07-07 19:10:20 +0900

    途中コミット

--- src/main/kotlin/net/testusuke/open/man10mail/EventListener.kt
+++ src/main/kotlin/net/testusuke/open/man10mail/EventListener.kt
@@ -1,6 +1,8 @@
 package net.testusuke.open.man10mail
 
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
 
 /**
  * Created by testusuke on 2020/07/04
@@ -8,4 +10,11 @@ import org.bukkit.event.Listener
  */
 object EventListener:Listener {
 
+    //  InventoryClick
+    @EventHandler
+    fun onInventoryClick(event:InventoryClickEvent){
+        val player = event.whoClicked
+        val inv = event.inventory
+
+    }
 }
