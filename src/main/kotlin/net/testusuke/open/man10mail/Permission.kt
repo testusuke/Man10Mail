a8068f411cc5144b8ffbc518b30b143a2753cf93
testusuke <49048811+testusuke@users.noreply.github.com>
2020-07-07 14:58:43 +0900

    Permissionの追加

---
+++ src/main/kotlin/net/testusuke/open/man10mail/Permission.kt
@@ -0,0 +1,11 @@
+package net.testusuke.open.man10mail
+
+/**
+ * Created on 2020/07/07
+ * Author testusuke
+ */
+object Permission {
+
+    const val GENERAL = "man10mail.general"
+    const val ADMIN = "man10mail.admin"
+}
