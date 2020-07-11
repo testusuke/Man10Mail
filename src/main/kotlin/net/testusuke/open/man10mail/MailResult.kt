c734e8a30a01362537d988c061eb9ab9ee4d0c47
owner <forgotten_saga_1129@outlook.jp>
2020-07-11 13:57:22 +0900

    MailResult.ktの追加

---
+++ src/main/kotlin/net/testusuke/open/man10mail/MailResult.kt
@@ -0,0 +1,13 @@
+package net.testusuke.open.man10mail
+
+/**
+ * Created by testusuke on 2020/07/11
+ * @author testusuke
+ */
+sealed class MailResult{
+    data class Success(val id: Int) : MailResult()
+    data class Error(val reason: MailErrorReason) : MailResult()
+}
+enum class MailErrorReason{
+    CAN_NOT_ACCESS_DB
+}
---
+++ src/main/kotlin/net/testusuke/open/man10mail/MailSenderType.kt
@@ -0,0 +1,9 @@
+package net.testusuke.open.man10mail
+
+/**
+ * Created by testusuke on 2020/07/10
+ * @author testusuke
+ */
+enum class MailSenderType {
+    PLAYER,SERVER,CUSTOM
+}
